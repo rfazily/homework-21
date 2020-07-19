29de712381b40590142330be4e50425848fe0400
Rayhana <rfazily@gmail.com>
2020-07-19 00:20:16 -0400

    fixed pages

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+            <div className="container text-primary">
+                <h1>Google Books Search</h1>
+            </div>
+    );
+}
+
+export default Header;
